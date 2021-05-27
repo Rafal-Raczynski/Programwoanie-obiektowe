039ff68d3044c389f3301f1127b62fd4baf1db43
Rafal-Raczynski <rafracz@o2.pl>
2021-05-27 11:09:04 +0200

    nic ciekawego

--- kraj.py
+++ kraj.py
@@ -1,7 +1,8 @@
 import sys
 from os import walk, path
+
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFileDialog, QHBoxLayout, QGroupBox, QVBoxLayout, \
-    QGridLayout, QLabel, QMainWindow, QFormLayout
+    QGridLayout, QLabel, QMainWindow, QFormLayout, QScrollArea
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 from matplotlib import pyplot as plt
@@ -11,8 +12,9 @@ class Covid(QMainWindow):
     def __init__(self, width, height):
         super().__init__()
         self.__init_view(width, height)
-        self.__prepare_buttons()
+        self.__prepare_import_button()
         self.__prepare_chart_panel()
+        self.update()
 
     def __prepare_chart_panel(self):
         fig = Figure(figsize=(5, 3), dpi=100)
@@ -30,25 +32,44 @@ class Covid(QMainWindow):
         self.setCentralWidget(elems)
         self.show()
 
-    def __prepare_buttons(self):
+    def __prepare_import_button(self):
         button = ButtonImport(".csv")
         self.__layout.addWidget(button, 10, 10)
+        self.__filepath = button.get_filepath()
+        print(self.__filepath)
+        # a = ReadData("/mnt/c/Kodowanie2/projekcik/Programowanie-obiektowe/time_series_covid19_confirmed_global.csv",
+        # "Poland")
+
+        # countries = a.get_list_of_all_countries()
+
+        #self.__layout.addWidget(button.scroll, 5, 10, 5, 2)
+
+    # self.__layout.addWidget(scroll, 1, 10, 9, 1)
+
+    # def __prepare_buttons2(self):
+    #     a = ReadData("time_series_covid19_confirmed_global.csv", "Poland")
+    #     scroll = ScrollButtons(a.get_list_of_all_countries())
+    #     self.__layout.addWidget(scroll, 1, 10, 9, 1)
 
 
 class ButtonImport(QPushButton):
     def __init__(self, accepted_formats):
         super().__init__("Import")
         self.__accepted_formats = accepted_formats
+        self.__filepath = None
+        self.scroll = None
         self.clicked.connect(self.__handle_select_file)
+
+    def get_filepath(self):
+        return self.__filepath
 
     def __handle_select_file(self):
         self.__filepath, _ = QFileDialog.getOpenFileName(self, "Select file")
         file_extension = path.splitext(self.__filepath)[1].lower()
         if file_extension not in self.__accepted_formats:
             print("Please choose file one more time")
-
-    def get_filename(self):
-        return self.__filepath
+        # Data = ReadData(self.__filepath, "Poland")
+        # self.scroll = ScrollButtons(Data.get_list_of_all_countries())
 
 
 class ReadData:
@@ -57,7 +78,6 @@ class ReadData:
         self.__list_of_countries = list()
         self.__read_all_countries_data()
         self.__selected_countries = selected_countries
-        self.display_selected_data()
 
     def __read_all_countries_data(self):
         i = 1
@@ -93,6 +113,7 @@ class ReadData:
         return n_of_patients_in_time
 
     def get_list_of_all_countries(self):
+        # print(self.__list_of_countries)
         return self.__list_of_countries
 
     def display_data(self, n_of_patients_in_countries):
@@ -112,13 +133,33 @@ class ReadData:
         self.display_data(countries_data)
 
 
+class ScrollButtons(QScrollArea):
+    def __init__(self, all_countries):
+        super().__init__()
+        self.__all_countries = all_countries
+        self.__init_view()
+
+    def __init_view(self):
+        btn_layout = QFormLayout()
+        btn_group = QGroupBox()
+
+        for i in self.__all_countries:
+            name = i
+            btn = QPushButton(name)
+            # btn.clicked.connect((lambda name_to_show: lambda _: print(name_to_show))(name))
+            btn_layout.addRow(btn)
+
+        btn_group.setLayout(btn_layout)
+        self.setWidget(btn_group)
+        self.setWidgetResizable(True)
+
+
 if __name__ == "__main__":
     app = QApplication([])
 
     # accepted_formats = (".jpg", ".png")
     img_browser = Covid(600, 300)
-    XD = ReadData("time_series_covid19_confirmed_global.csv", ["Poland", "Russia"])
-    XD.read_countries_data()
-    # print(XD.get_list_of_all_countries())  # .keys())
+    # XD = ReadData("/mnt/c/Kodowanie2/projekcik/Programowanie-obiektowe/time_series_covid19_confirmed_global.csv",
+    #               ["Poland", "Russia"])
 
     sys.exit(app.exec_())
