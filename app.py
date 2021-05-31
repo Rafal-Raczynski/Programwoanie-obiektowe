16dc39f635f5c409c8d2e22b234fb097045e8c59
Rafal-Raczynski <73250226+Rafal-Raczynski@users.noreply.github.com>
2021-05-31 18:22:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from plot import Plot
 from pdf_generator import PdfReportGenerator
 
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFileDialog, QHBoxLayout, QGroupBox, QVBoxLayout, \
-    QGridLayout, QLabel, QMainWindow, QFormLayout, QScrollArea, QMessageBox, QLineEdit
+    QGridLayout, QLabel, QMainWindow, QFormLayout, QScrollArea, QMessageBox, QLineEdit, QSlider
 from reportlab.lib.utils import ImageReader
 
 
@@ -34,12 +34,18 @@ class Covid(QMainWindow):
             if button.handle_select_file() == 0:
                 break
         a = ReadData(button.get_filepath())
-        scroll = ScrollButtons(a.get_list_of_all_countries(), button.get_filepath())
+        print(a.get_amount_of_days())
+        plot = Plot(button.get_filepath())
+        plot.set_x_lim(100, 200)
+        scroll = ScrollButtons(a.get_list_of_all_countries(), button.get_filepath(),
+                               plot)
+        slider = Slider()
+        self.__layout.addWidget(scroll, 2, 15, 6, 3)
         filtr = Filtr(a.get_list_of_all_countries(), button.get_filepath(), scroll)
         self.__layout.addWidget(filtr, 0, 15, 1, 3)
         self.__layout.addWidget(filtr.button, 1, 15, 1, 3)
-        self.__layout.addWidget(scroll, 2, 15, 6, 3)
-        self.__layout.addWidget(scroll.data, 0, 0, 8, 5)
+        self.__layout.addWidget(plot, 0, 0, 8, 5)
+        # self.__layout.addWidget(slider, 9, 0, 5, 1)
 
     def __prepare_pdf_button(self):
         name = "Export to PDF"
@@ -47,19 +53,10 @@ class Covid(QMainWindow):
         pdf_button = PdfSaveButton(name, graph)
         self.__layout.addWidget(pdf_button, 10, 15)
 
-    @staticmethod
-    def handle_selected_countries(name, data):
-        print("Clicked:", name)
-        if name in data.selected_countries:
-            data.remove_country(name)
-        else:
-            data.add_country(name)
-        data.display_selected_data()
-        data.show()
-
 
 class ReadData:
     def __init__(self, filepath):
+        self.__amount_of_days = None
         self.__filepath = filepath
         self.__list_of_countries = list()
         self.__read_all_countries_data()
@@ -71,9 +68,14 @@ class ReadData:
                 possible_region = line.split(",")[0]
                 if i != 1 and possible_region == "":
                     country = line.split(",")[1]
+                    if self.__amount_of_days is None:
+                        self.__amount_of_days = len(line.split(",")[4:])
                     self.__list_of_countries.append(country)
 
                 i = i + 1
+
+    def get_amount_of_days(self):
+        return self.__amount_of_days
 
     def get_list_of_all_countries(self):
         return self.__list_of_countries
@@ -106,10 +108,11 @@ class ButtonImport(QPushButton):
 
 
 class ScrollButtons(QScrollArea):
-    def __init__(self, all_countries, filepath):
+    def __init__(self, all_countries, filepath, plot: Plot):
         super().__init__()
+        self.__filepath = filepath
         self.__all_countries = all_countries
-        self.data = Plot(filepath)
+        self.__data = plot
         self.__init_view()
 
     def __init_view(self):
@@ -119,7 +122,7 @@ class ScrollButtons(QScrollArea):
         for i in self.__all_countries:
             name = i
             btn = QPushButton(name)
-            btn.clicked.connect(lambda checked, n=name: Covid.handle_selected_countries(n, self.data))
+            btn.clicked.connect(lambda checked, n=name: self.handle_selected_countries(n))
             btn_layout.addRow(btn)
         btn_group.setLayout(btn_layout)
         self.setWidget(btn_group)
@@ -128,6 +131,16 @@ class ScrollButtons(QScrollArea):
     def set_all_countries(self, countries):
         self.__all_countries = countries
         self.__init_view()
+
+    def handle_selected_countries(self, name):
+        print("Clicked:", name)
+        if name in self.__data.selected_countries:
+            self.__data.remove_country(name)
+        else:
+            self.__data.add_country(name)
+        self.__data.display_selected_data()
+        self.__data.show()
+
 
 class Filtr(QLineEdit):
     def __init__(self, all_countries, filepath, scroll: ScrollButtons):
@@ -150,6 +163,11 @@ class Filtr(QLineEdit):
 
     def __update_buttons(self):
         self.scroll.set_all_countries(self.__filtred_countries)
+
+
+class Slider(QSlider):
+    def __init__(self):
+        super().__init__()
 
 
 class PdfSaveButton(QPushButton):
